c7287ce79ba2d51a856a6b12b588f14610e2cd17
Roshan Jha <roshan@betterhalf.ai>
2023-01-19 17:29:32 +0530

    [refactor]: changed folder name and introduced file tests

---
+++ test/5_fifthParallelTest.js
@@ -0,0 +1,28 @@
+const { Builder, Key, By } = require("selenium-webdriver");
+const should = require("chai").should();
+
+// Describe Block : For a group of tests
+// It block : For an individual test
+
+describe("getting restarted with todo tests in parallel manner", () => {
+  it("should add a new todo", async () => {
+    const driver = await new Builder().forBrowser("chrome").build();
+    try {
+      await driver.get("https://lambdatest.github.io/sample-todo-app");
+      await driver
+        .findElement(By.id("sampletodotext"))
+        .sendKeys("Learn Selenium", Key.ENTER);
+      const todoText = await driver
+        .findElement(By.xpath("//li[last()]"))
+        .getText();
+      todoText.should.equal("Learn Something");
+    } catch (error) {
+      console.error("An error occurred while executing the test");
+    } finally {
+      await driver.quit();
+    }
+  });
+});
+
+// Renaming the folder to "test" can actually help out in automating the process of writing tests
+// And in package.json file introduce a new script test:"mocha --no-timeouts"
